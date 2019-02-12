b268b7185a70e22537a19b51990d26c789200952
wuxing <wuxing@u51.com>
2019-02-12 12:17:23 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+//const isArray = require('lodash').isArray;
+
+exports.handler = async (event) => {
+    
+    console.log("Lambda process");
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
